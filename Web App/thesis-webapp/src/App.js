465d92d5cc56c843aa749e0e73fd4e3c45e2134f
ImperialDuck-dev <mjmuyco13@gmail.com>
2023-11-03 04:33:17 +0800

    refactoring

--- Web App/thesis-webapp/src/App.js
+++ Web App/thesis-webapp/src/App.js
@@ -1,7 +1,5 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
   );
 }
