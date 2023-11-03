54377f456aa19c32d19a4238910eade74e4b7cb7
DyanMarku <haberjohnmarkk@gmail.com>
2023-11-03 21:02:47 +0800

    - updated the errors on HomePage.js

--- Web-App/thesis-webapp/src/pages/HomePage.js
+++ Web-App/thesis-webapp/src/pages/HomePage.js
@@ -16,13 +16,9 @@ function HomePage() {
             <h1 className="text-[#F19C1B] font-bold text-9xl pb-8 text-justify">
               RAIL DETECT
             </h1>
-          <div className="absolute top-0 -right-10">
-            <img src={camera} alt="camera"></img>
-          </div>
-            
-        </div>
+
             <div className="absolute top-0 -right-10">
-              <img src="img/camera.svg" alt="camera"></img>
+              <img src={camera} alt="camera"></img>
             </div>
           </div>
 
@@ -48,21 +44,10 @@ function HomePage() {
 
       <div className="w-2/5 relative overflow-hidden">
         <div className="absolute -bottom-20 -right-20">
-          <img src={train}  alt="train"></img>
+          <img src={train} alt="train"></img>
         </div>
       </div>
     </div>
-  );
-}
-
-function HomePageButton(props) {
-  return (
-    <button className="text-2xl font-bold bg-[#3A623F] hover:bg-[#2c4a30]  border-4 border-[#F19C1B] rounded-full text-white px-2 py-1 shadow-[0_0_69px_3px_rgba(0,255,255,0.25)]">
-      <div className="flex flex-row align-middle justify-center space-x-2">
-        {props.children}
-        <img src="img/angle-right.svg" alt="angle-right"></img>
-      </div>
-    </button>
   );
 }
 
