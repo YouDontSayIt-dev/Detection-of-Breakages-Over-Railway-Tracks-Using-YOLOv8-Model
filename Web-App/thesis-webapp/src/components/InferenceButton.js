2687342d5cd39ecc22a566602905984f6ad116bb
blancojames88 <blancojames88@gmail.com>
2023-11-03 21:03:04 +0800

    added all button components

---
+++ Web-App/thesis-webapp/src/components/InferenceButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InferenceButton = () => {
+  return (
+    <button className="w-[300px] h-[64px] bg-custom-h1-text text-2xl font-custom text-custom-white px-8 py-2 rounded-md shadow-custom-box-shadow-inference-btn transition active:translate-y-1">
+      Run Inference
+    </button>
+  );
+};
+
+export default InferenceButton;
