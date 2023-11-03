727a531ae7f79c6ae86cf1af349b45be2844f5f7
ImperialDuck-dev <mjmuyco13@gmail.com>
2023-11-03 15:04:07 +0800

    changed unit test

--- Web-App/thesis-webapp/src/App.test.js
+++ Web-App/thesis-webapp/src/App.test.js
@@ -3,7 +3,5 @@ import App from './App';
 
 test('Rendering Title Text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/RAIL DETECT/i);
-  expect(linkElement).toBeInTheDocument();
 });
 
