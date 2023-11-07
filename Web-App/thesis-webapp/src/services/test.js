6949e5cb7a697101294ebacaeea2942d9f3bc408
blancojames88 <blancojames88@gmail.com>
2023-11-07 14:57:19 +0800

    added initial api testing

---
+++ Web-App/thesis-webapp/src/services/test.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const fs = require("fs");
+
+const image = fs.readFileSync("../assets/broken_rail.png", {
+  encoding: "base64",
+});
+
+axios({
+  method: "POST",
+  url: "https://detect.roboflow.com/railway-crack-detection/10",
+  params: {
+    api_key: "j4oHBD3msAlUlJvXwsHz",
+  },
+  data: image,
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+})
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error.message);
+  });
