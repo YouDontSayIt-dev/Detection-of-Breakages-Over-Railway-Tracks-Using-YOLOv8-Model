8c148cf833a035622102c5cc7578cc5480ef35f7
blancojames88 <blancojames88@gmail.com>
2023-11-08 13:10:14 +0800

    converted user uploads to base64

--- Web-App/thesis-webapp/src/components/UploadButton.js
+++ Web-App/thesis-webapp/src/components/UploadButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const UploadButton = ({ onImageChange }) => {
+const UploadButton = ({ onChange }) => {
   return (
     <div className="grid w-full max-w-xs items-center gap-1.5">
       <label htmlFor="picture" style={{ color: "white" }}>
@@ -9,6 +9,7 @@ const UploadButton = ({ onImageChange }) => {
       <input
         id="picture"
         type="file"
+        accept="image/*"
         className="flex h-13 w-72 rounded-md px-3 py-3 text-white text-medium file:border-1 file:bg-transparent file:text-white file:text-medium file:font-medium"
         style={{
           backgroundColor: "#3A623F",
@@ -16,7 +17,7 @@ const UploadButton = ({ onImageChange }) => {
           cursor: "pointer",
           borderRadius: "10px",
         }}
-        onChange={onImageChange} // Call the provided function on change
+        onChange={onChange} // Call the provided function on change
       />
     </div>
   );
--- Web-App/thesis-webapp/src/services/test.js
+++ Web-App/thesis-webapp/src/services/test.js
@@ -1,25 +1,16 @@
 const axios = require("axios");
-const fs = require("fs");
-
-const image = fs.readFileSync("../assets/broken_rail.png", {
-  encoding: "base64",
-});
 
 axios({
   method: "POST",
   url: "https://detect.roboflow.com/railway-crack-detection/10",
   params: {
     api_key: "j4oHBD3msAlUlJvXwsHz",
-  },
-  data: image,
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
+    image:
+      "https://i.cbc.ca/1.4511754.1517408760!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_620/cracked-rail.jpg",
   },
 })
   .then(function (response) {
-    const outputData = JSON.stringify(response.data, null, 2);
-    fs.writeFileSync("output.json", outputData);
-    console.log("Output data has been written to output.json");
+    console.log(response.data);
   })
   .catch(function (error) {
     console.log(error.message);
