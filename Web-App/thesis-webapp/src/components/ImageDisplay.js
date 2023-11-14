74b089125b9ffbd08a8aa942ba1efab740125bc0
blancojames88 <blancojames88@gmail.com>
2023-11-14 19:29:16 +0800

    update responsive container (nyd)

--- Web-App/thesis-webapp/src/components/ImageDisplay.js
+++ Web-App/thesis-webapp/src/components/ImageDisplay.js
@@ -15,12 +15,16 @@ const ImageDisplay = ({ selectedImage, drawLine, bboxData }) => {
       // Load the image onto the canvas
       const img = new Image();
       img.src = selectedImage;
+      // Update canvas dimensions based on image size
+      setCanvasWidth(img.width);
+      setCanvasHeight(img.height);
       img.onload = () => {
-        // Update canvas dimensions based on image size
-        setCanvasWidth(img.width);
-        setCanvasHeight(img.height);
+        // Set the canvas dimensions
+        canvas.width = img.width;
+        canvas.height = img.height;
 
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        // Draw the image with the updated dimensions
+        ctx.drawImage(img, 0, 0);
 
         // Draw bounding boxes if bboxData is available
         if (bboxData && bboxData.predictions) {
@@ -35,8 +39,6 @@ const ImageDisplay = ({ selectedImage, drawLine, bboxData }) => {
 
             const x1 = x - width / 2;
             const y1 = y - height / 2;
-            // const x2 = x + width / 2;
-            // const y2 = y + height / 2;
 
             ctx.beginPath();
             ctx.rect(x1, y1, width, height);
