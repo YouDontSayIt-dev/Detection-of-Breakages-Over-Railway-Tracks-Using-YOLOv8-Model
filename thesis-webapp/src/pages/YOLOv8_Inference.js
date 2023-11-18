ff48b53fc44cde9145c5b6f535efa7939733bd70
blancojames88 <blancojames88@gmail.com>
2023-11-18 19:53:54 +0800

    started inference page

--- thesis-webapp/src/pages/YOLOv8_Inference.js
+++ thesis-webapp/src/pages/YOLOv8_Inference.js
@@ -3,11 +3,16 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 
 const Yolov8Inference = () => {
-  const location = useLocation();
+  const location = useLocation(); //gets the current path location
 
   return (
-    <div className="w-screen h-screen bg-customBackground overflow-hidden">
+    <div className="flex w-screen h-screen bg-customBackground overflow-hidden">
       <Sidebar activePage={location.pathname} />
+      <div className="w-[20%] h-screen bg-black"></div>
+      <div className="w-[20%] h-screen bg-red-500"></div>
+      <div className="w-[20%] h-screen bg-orange-500"></div>
+      <div className="w-[20%] h-screen bg-green-500"></div>
+      <div className="w-[20%] h-screen bg-blue-500"></div>
     </div>
   );
 };
